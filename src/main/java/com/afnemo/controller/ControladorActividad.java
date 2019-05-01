b440bed1fea08c056a9278a463c3c36348dfe223
Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-05-01 10:59:58 -0500

    Update procfile

---
+++ src/main/java/com/afnemo/controller/ControladorActividad.java
@@ -0,0 +1,13 @@
+package com.afnemo.controller;
+/**
+  * 
+  * @autor:       Arnold Herrera
+  * @date:        1/05/2019
+  * @filename:    ControladorActividad.java
+  * @Copyright (C) 2019 Asociación Afrocultural Neftalí Mosquera (afnemo)
+  * 
+  */
+
+public class ControladorActividad {
+
+}
